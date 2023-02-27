626d2df36d0e1672c8b695ae4395794db8ecd718
kateizev21 <kateizev@gmail.com>
2023-02-27 23:03:07 +0000

    great

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Tesla project is ready for the market"
+echo "Deploment started"
+mkdir deployment
+mkdir app.java
+touch deployment/app.java
+chmod 744 deployment/app.java
+chmod obi:tesla deployment/app.java
+cp deployment/app.java app
+echo "deployment successful"
+whoami
+echo "Congratulation you are deployed"
+echo "you shall successed in Jesus name"
